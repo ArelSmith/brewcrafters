8db0c98184b538fab2f3bcf17a0d0f3c944ed512
Shida <wonderfulofgame@gmail.com>
2025-10-02 02:37:20 +0700

    Adding roles system in the project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,86 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $permissions = [
+            'users' => [
+                'create-user',
+                'view-user',
+                'edit-user',
+                'delete-user',
+            ],
+            'products' => [
+                'create-product',
+                'edit-product',
+                'delete-product',
+                'view-product',
+            ],
+            'posts' => [
+                'create-post',
+                'edit-post',
+                'delete-post',
+                'view-post',
+            ],
+            'orders' => [
+                'create-order',
+                'edit-order',
+                'delete-order',
+                'view-order',
+            ],
+            'categories' => [
+                'create-category',
+                'edit-category',
+                'delete-category',
+                'view-category',
+            ],
+            'settings' => [
+                'edit-settings',
+            ]
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $roles = [
+            'admin',
+            'writer',
+            'member',
+            'guest',
+        ];
+
+        foreach($roles as $role) {
+            Role::create([
+                'name' => $role,
+                'guard_name' => 'web',
+            ]);
+        }
+
+        foreach($permissions as $group => $perms) {
+            foreach($perms as $perm) {
+                Permission::firstOrCreate([
+                    'name' => $perm,
+                    'guard_name' => 'web',
+                ]);
+            }
+        }
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $adminRole->givePermissionTo(Permission::all());
+
+        $writerRole = Role::where('name', 'writer')->first();
+        $writerRole->givePermissionTo(['create-post', 'edit-post', 'delete-post', 'view-post']);
+
+        $guestRole = Role::where('name', 'guest')->first();
+        $guestRole->givePermissionTo(['view-post', 'view-product', 'edit-settings']);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@brewcrafters.test',
+            'password' => Hash::make('admin#1234'),
+        ])->assignRole('admin');
+
+        User::create([
+            'name' => 'user',
+            'email' => 'user@brewcrafters.test',
+            'password' => Hash::make('user#1234'),
+        ])->assignRole('guest');
+
+
     }
 }
